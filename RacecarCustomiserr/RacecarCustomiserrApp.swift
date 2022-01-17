ede95b212b9975b5f289d3b5a2433eaac75716ee
Killian James <james.k@etoncollege.org.uk>
2022-01-17 21:27:08 +0000

    Initial Commit

---
+++ RacecarCustomiserr/RacecarCustomiserrApp.swift
@@ -0,0 +1,17 @@
+//
+//  RacecarCustomiserrApp.swift
+//  RacecarCustomiserr
+//
+//  Created by James, Killian (NA) on 17/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct RacecarCustomiserrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
